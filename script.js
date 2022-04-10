3a7575cbb6a94baf14b86b691299b14cecfa3159
waleed smadi <waleed0smadi@outlook.com>
2022-04-10 22:30:12 +0300

    Reorder the code / edit a comment

--- script.js
+++ script.js
@@ -4,10 +4,38 @@ const theTemplete = document.querySelector("template"),
       inputSearch = document.querySelector("#search-input");
 
 
-      
+
 // array to put names, emails and user div
 // to find the text that user write
 let usersArr = [];
+
+
+
+// bring names & emails from placeholder JSON
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(res => {
+    return res.json();
+})
+
+.then(users => {
+    /*
+    1 - make a copy of user div
+    2 - bring name and email from user div
+    3 - put name and email inside the user div
+    4 - append the div user to the wrapper to show it
+    5 - return the name, email and the user div in an array
+    */
+    usersArr =  users.map(user => {
+        const cloneUser = theTemplete.content.cloneNode(true).children[0];
+        const userName = cloneUser.querySelector(".name");
+        const userEmail = cloneUser.querySelector(".email");
+        userName.innerText = user["name"];
+        userEmail.innerText = user["email"];
+        usersWrapper.appendChild(cloneUser);
+        return {"name": user["name"], "email": user["email"], element: cloneUser};
+    });
+});
+
 
 
 
@@ -26,27 +54,3 @@ inputSearch.addEventListener("input", function(e){
 
 
 
-// bring names & emails from placeholder JSON
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(res => {
-    return res.json();
-})
-
-.then(users => {
-    /*
-    1 - make a copy of user div
-    2 - bring name and email from user div
-    3 - put name and email inside the user div
-    4 - append the div user to the wrapper show it
-    5 - return the name, email and the user div in an array
-    */
-    usersArr =  users.map(user => {
-        const cloneUser = theTemplete.content.cloneNode(true).children[0];
-        const userName = cloneUser.querySelector(".name");
-        const userEmail = cloneUser.querySelector(".email");
-        userName.innerText = user["name"];
-        userEmail.innerText = user["email"];
-        usersWrapper.appendChild(cloneUser);
-        return {"name": user["name"], "email": user["email"], element: cloneUser};
-    });
-});
